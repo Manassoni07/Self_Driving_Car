af052a929b9d0667d5752ba1d36788b193f19c8d
Manas <manassoni1311@gmail.com>
2025-07-19 21:57:29 +0530

    trigonometry in car physics is added

--- car.js
+++ car.js
@@ -13,10 +13,13 @@ class Car {
 
         this.controls = new Controls();// now we add new control.js file to index.html file 
 
-
     }
 
-    update(){
+    update() {
+        this.#move();
+    }
+
+    #move() {
         if (this.controls.forward) {
             this.speed += this.acceleration;
 
@@ -24,42 +27,54 @@ class Car {
         if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
-        if (this.speed>this.maxSpeed) {
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        if (this.speed<-this.maxSpeed/2) {
-            this.speed = -this.maxSpeed/2; // -ve sign indicate opposite direction
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2; // -ve sign indicate opposite direction
         }
 
-        if (this.speed>0) {
+        if (this.speed > 0) {
             this.speed -= this.friction;
         }
-        if (this.speed<0) {
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if (Math.abs(this.speed)<this.friction) {
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-        if (this.controls.left) {
-            this.angle += 0.03;
-        }
-        if (this.controls.right) {
-            this.angle -= 0.03;
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1; // this will help car in reverse movements 
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
         }
 
-        this.y -= this.speed; 
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+        // this.y -= this.speed; this not need more
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height
         );
 
         ctx.fill();
+
+        ctx.restore();
     }
 
 }
