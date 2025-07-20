e1ebdd8aebdea8d78ce07ac69ca2263f1d1dc621
Manas <manassoni1311@gmail.com>
2025-07-20 16:16:18 +0530

    Creating Road

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(100,100,30,50); // x,y,width,height
 car.draw(ctx);// drawing car using this context 
 // but we don't have a car so we define car object by import new file car.js in index.html
@@ -12,7 +13,10 @@ animate();
 
 function animate(){
     car.update();
+
     canvas.height = window.innerHeight; // this will solve car issue of extend 
+    road.draw(ctx);
     car.draw(ctx);
+
     requestAnimationFrame(animate); // it make loop 
 }
