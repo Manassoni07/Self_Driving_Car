46dc15b9dfe44eff9353be71fbd2a5de00515e82
Manas <manassoni1311@gmail.com>
2025-07-21 16:24:48 +0530

    casting Rays are added

--- car.js
+++ car.js
@@ -11,12 +11,14 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();// now we add new control.js file to index.html file 
 
     }
 
     update() {
         this.#move();
+        this.sensor.update();
     }
 
     #move() {
@@ -75,6 +77,8 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 
 }
